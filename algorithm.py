27be7923083c59d68a28519dd58862b4b5b694ef
Akram Abdoul-Wahab <akram.abdl@outlook.fr>
2023-03-31 19:41:16 +0200

    first commit

---
+++ algorithm.py
@@ -0,0 +1,96 @@
+# algorithms.py
+from typing import Callable
+
+
+class SortingAlgorithm:
+    def __init__(self, name: str, function: Callable):
+        self.name = name
+        self.function = function
+
+
+def bubble_sort(arr, visualize):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+                visualize(arr)
+
+
+def selection_sort(arr, visualize):
+    n = len(arr)
+    for i in range(n):
+        min_idx = i
+        for j in range(i+1, n):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+        visualize(arr)
+
+
+def insertion_sort(arr, visualize):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i-1
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+            visualize(arr)
+        arr[j + 1] = key
+
+
+def merge_sort(arr, visualize):
+    def merge(left, right):
+        result = []
+        i = j = 0
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                result.append(left[i])
+                i += 1
+            else:
+                result.append(right[j])
+                j += 1
+        result.extend(left[i:])
+        result.extend(right[j:])
+        return result
+
+    def _merge_sort(arr):
+        if len(arr) <= 1:
+            return arr
+        mid = len(arr) // 2
+        left = _merge_sort(arr[:mid])
+        right = _merge_sort(arr[mid:])
+        merged = merge(left, right)
+        visualize(merged)
+        return merged
+
+    return _merge_sort(arr)
+
+
+def quick_sort(arr, visualize):
+    def _quick_sort(arr, low, high):
+        if low < high:
+            pivot_idx = partition(arr, low, high)
+            _quick_sort(arr, low, pivot_idx)
+            _quick_sort(arr, pivot_idx + 1, high)
+
+    def partition(arr, low, high):
+        pivot = arr[low]
+        left = low + 1
+        right = high
+        done = False
+        while not done:
+            while left <= right and arr[left] <= pivot:
+                left = left + 1
+            while arr[right] >= pivot and right >= left:
+                right = right - 1
+            if right < left:
+                done = True
+            else:
+                arr[left], arr[right] = arr[right], arr[left]
+                visualize(arr)
+        arr[low], arr[right] = arr[right], arr[low]
+        visualize(arr)
+        return right
+
+    _quick_sort(arr, 0, len(arr) - 1)
---
+++ main.py
@@ -0,0 +1,47 @@
+import random
+import matplotlib.pyplot as plt
+from algorithm import SortingAlgorithm, bubble_sort, selection_sort, insertion_sort, merge_sort, quick_sort
+
+
+class SortingVisualizer:
+    def __init__(self, array_size=50):
+        self.array_size = array_size
+        self.array = [random.randint(1, 100) for _ in range(array_size)]
+
+    def visualize(self, arr):
+        plt.clf()
+        plt.bar(range(len(arr)), arr)
+        plt.pause(0.05)
+
+    def run(self, sorting_algorithm: SortingAlgorithm):
+        plt.ion()
+        plt.title(sorting_algorithm.name)
+        sorting_algorithm.function(self.array, self.visualize)
+        plt.show(block=True)
+
+
+def main():
+    sorting_algorithms = [
+        SortingAlgorithm("Bubble Sort", bubble_sort),
+        SortingAlgorithm("Selection Sort", selection_sort),
+        SortingAlgorithm("Insertion Sort", insertion_sort),
+        SortingAlgorithm("Merge Sort", merge_sort),
+        SortingAlgorithm("Quick Sort", quick_sort),
+    ]
+
+    visualizer = SortingVisualizer()
+
+    print("Available sorting algorithms:")
+    for index, algorithm in enumerate(sorting_algorithms):
+        print(f"{index + 1}. {algorithm.name}")
+
+    selected_algorithm = int(
+        input("Enter the number of the algorithm you want to visualize: ")) - 1
+    algorithm = sorting_algorithms[selected_algorithm]
+
+    print(f"Visualizing {algorithm.name}...")
+    visualizer.run(algorithm)
+
+
+if __name__ == "__main__":
+    main()
